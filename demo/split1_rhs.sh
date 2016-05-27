d2aed89e9449b7f300c6d10a7b6acf3e2936e21e
Dan Osborne <djosborne10@gmail.com>
2016-05-27 11:14:37 -0700

    Use same redis command

--- demo/split1_rhs.sh
+++ demo/split1_rhs.sh
@@ -2,10 +2,6 @@
 
 . $(dirname ${BASH_SOURCE})/util.sh
 
-export SLAVE_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mesoscni_slave_1)
-export ETCD_AUTHORITY=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mesoscni_etcd_1):2379
-export MARATHON_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mesoscni_marathon_1)
-
 desc "I can access the frontend"
 run "echo '<----- see left pane'"
 desc "But I can also access redis"
@@ -22,5 +18,5 @@ run "cat $(relative frontend-policy.yaml)"
 run "./calicoctl create --filename=./frontend-policy.yaml"
 
 desc "We can no longer access redis directly - only the frontend can"
-run "docker exec mesoscni_client_1 docker run -i  --rm redis:alpine redis-cli -h database.marathon.mesos -p 6379 ping"
+run "docker exec mesoscni_client_1 docker run --rm redis:alpine redis-cli -h database.marathon.mesos -p 6379 SET hits 0"
 
